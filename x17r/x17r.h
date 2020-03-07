8d912974e5cd14c4a2c3d0603b21330ea5075bd1
Caster <lxzan520@gmail.com>
2020-03-07 16:52:03 +0800

    save

--- x17r/x17r.h
+++ x17r/x17r.h
@@ -10,7 +10,7 @@ extern "C"{
 #include <stddef.h>
 #include "sph_types.h"
 
-static void getAlgoString(const uint8_t* prevblock, char *output);
+//static void getAlgoString(const uint8_t* prevblock, char *output);
 
 void x17r_hash(void* output, const void* input, const int in_len);
 
